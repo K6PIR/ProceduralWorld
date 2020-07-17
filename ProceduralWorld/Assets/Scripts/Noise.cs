6f71b6dff27d441b385a0df3234384ddfb16b104
K6PIR <kellian.cipierre@gmail.com>
2020-07-18 00:30:56 +0200

    update Rider Modules

---
+++ ProceduralWorld/Assets/Scripts/Noise.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise
+{
+    public static float[,] GenerateNoiseMap(int weight, int height)
+    {
+        
+    } 
+}
